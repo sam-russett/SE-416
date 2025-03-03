34896737b1f1f927b407180420eecbf113997c00
sam-russett <sam@russetts.com>
2025-03-02 19:20:49 -0600

    example usages added in main file

---
+++ Assignment_1/Main.java
@@ -0,0 +1,19 @@
+package Assignment_1;
+
+// Some example use cases of the Data and Stamp coupling
+public class Main {
+    public static void main(String[] args) {
+        car myCar = new car("Mustang", 120, "Ford", "GT");
+        Airplane myPlane = new Airplane("Boeing 747", "35000 ft", 570);
+        
+        UpdateVehicle service = new UpdateVehicle();
+
+        // Stamp coupling: passing objects and speed values as parameters
+        service.updateSpeed(myCar, 150);
+        service.updateSpeed(myPlane, 600);
+
+        //Data coupling: passing in integer values as arguments to another method
+        int distanceTraveled = myCar.getDistance(myCar.getSpeed(), 2);
+        System.out.println("Maximum distance example car can travel in 2 hours: " + distanceTraveled + " miles");
+    }
+}
